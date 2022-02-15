68fc19b1f33de52f546507bf528907d3d11b8b66
stalekc <stalekc@gmail.com>
2022-02-15 18:53:08 +0300

    lr 1 var 1 task 4

---
+++ lr1/src/lr113/lr114.java
@@ -0,0 +1,17 @@
+package lr113;
+
+import java.util.Scanner;
+
+public class lr114 {
+    public static void main(String[] args) {
+        String example = "password";
+        Scanner in = new Scanner(System.in);
+        String test_str = in.next();
+        if (example.equals(test_str)){
+            System.out.println("same");
+        }
+        else{
+            System.out.println("ne same");
+        }
+    }
+}
