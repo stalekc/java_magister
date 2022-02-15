4d9940454ca427a66413ee7ce7d438887b1ead57
stalekc <stalekc@gmail.com>
2022-02-15 18:46:53 +0300

    lr 1 var 1 task 3

---
+++ lr1/src/lr113/lr113.java
@@ -0,0 +1,18 @@
+package lr113;
+
+import java.util.Scanner;
+
+public class lr113 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = "Hello";
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print(str);
+        }
+        System.out.println();
+        for (int i = 0; i < n; i++) {
+            System.out.println(str);
+        }
+    }
+}
